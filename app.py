8a59be65497c86e96af3c9b80c15d9612445f344
vivekmishra89 <vimis@microsoft.com>
2022-05-31 21:00:58 +0530

    Updated the code with KV

--- app.py
+++ app.py
@@ -1,46 +1,27 @@
 from datetime import datetime
+from urllib import response
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
-from azure.core.exceptions import HttpResponseError
+from azure.identity import DefaultAzureCredential,AzureCliCredential
+from azure.core.exceptions import ClientAuthenticationError
 
-
-
-app = Flask(__name__)
-
+app = Flask(__name__)	
 
 @app.route('/')
 def index():
     vault_uri = "https://neo-rbac-webapp-kv.vault.azure.net/"
     credential = DefaultAzureCredential()
-    client = SecretClient(vault_uri=vault_uri,
-                  credential=credential)
-    existing_secret = client.get_secret('2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b')
-    print('Request for index page received')
-    print(existing_secret)
-    return render_template('index.html')
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-
-@app.route('/hello', methods=['POST'])
-def hello():
-    name = request.form.get('name')
-
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return render_template('hello.html', name=name)
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return redirect(url_for('index'))
-
-    
+    client = SecretClient(vault_url=vault_uri, credential=credential)
+    try:
+        secret = client.get_secret("2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b")
+        print(secret.Value)
+        response = f'secret{str(secret.value)}'
+        return response
+        
+    except ClientAuthenticationError as ex:
+        print(ex.message)
+        return ex.message
+        
 
 if __name__ == '__main__':
-
     app.run()
-
