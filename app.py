8b784b8e48e99906e22437002ee7d11aa1f9c7e9
vivekmishra89 <vimis@microsoft.com>
2022-05-31 23:56:10 +0530

    Updated token code

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential,AzureCliCredential
 from azure.core.exceptions import ClientAuthenticationError
+import adal
 
 app = Flask(__name__)	
 
@@ -13,15 +14,14 @@ def index():
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=vault_uri, credential=credential)
     secret_name = '2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b'
-    #print("\n.. Create Secret")
-    #expires = datetime.now() + timedelta(days=365)
-    #secret = client.set_secret("helloWorldSecretName", "helloWorldSecretValue", expires_on=expires)
-    #print("Secret with name '{0}' created with value '{1}'".format(secret.name, secret.value))
-    #print("Secret with name '{0}' expires on '{1}'".format(secret.name, secret.properties.expires_on))
+
     try:
         bank_secret = client.get_secret(secret_name)
-        print(bank_secret.value)
-        response = str("Secret name is:" + secret_name + " and secret value is " + str(bank_secret.value))
+        authority = ('https://login.microsoftonline.com/e18a0c35-c3ed-46f4-8e69-018ca67f8288')
+        context = adal.AuthenticationContext(authority)
+        token = context.acquire_token_with_client_credentials("https://management.azure.com",bank_secret.name , bank_secret.value)
+        print(token)
+        response = str("Secret name is:" + secret_name + " and secret value is " + str(bank_secret.value) + "and token is " + str(token))
         
         return response
         
