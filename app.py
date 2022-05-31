18ce895b3448b294faf12b54a40c2930575071ee
vivekmishra89 <vimis@microsoft.com>
2022-05-31 23:36:03 +0530

    Updated secret code

--- app.py
+++ app.py
@@ -12,15 +12,17 @@ def index():
     vault_uri = "https://neo-rbac-webapp-kv.vault.azure.net/"
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=vault_uri, credential=credential)
-    print("\n.. Create Secret")
-    expires = datetime.now() + timedelta(days=365)
-    secret = client.set_secret("helloWorldSecretName", "helloWorldSecretValue", expires_on=expires)
-    print("Secret with name '{0}' created with value '{1}'".format(secret.name, secret.value))
-    print("Secret with name '{0}' expires on '{1}'".format(secret.name, secret.properties.expires_on))
+    secret_name = '2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b'
+    #print("\n.. Create Secret")
+    #expires = datetime.now() + timedelta(days=365)
+    #secret = client.set_secret("helloWorldSecretName", "helloWorldSecretValue", expires_on=expires)
+    #print("Secret with name '{0}' created with value '{1}'".format(secret.name, secret.value))
+    #print("Secret with name '{0}' expires on '{1}'".format(secret.name, secret.properties.expires_on))
     try:
-        bank_secret = client.get_secret(secret.name)
-        print(secret)
-        response = f'secret{str(secret)}'
+        bank_secret = client.get_secret(secret_name)
+        print(bank_secret.value)
+        response = str("Secret name is:" + secret_name + " and secret value is " + str(bank_secret.value))
+        
         return response
         
     except ClientAuthenticationError as ex:
