824ca667c6f61f56f7a88d2500dbe6d26f6ef5f7
vivekmishra89 <vimis@microsoft.com>
2022-06-02 18:28:33 +0530

    Updated graph code

--- app.py
+++ app.py
@@ -33,10 +33,12 @@ print(token)
 ###Get token for Graph to get user name from principle ID###
 
 graph_token_uri = "https://login.microsoftonline.com/e18a0c35-c3ed-46f4-8e69-018ca67f8288/oauth2/v2.0/token"
-graph_req_headers = {'Content-Type': 'x-www-form-urlencoded'}
-graph_req_body = 'grant_type=client_credentials&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&scope=https://graph.microsoft.com/.default&client_secret=' + str(bank_secret)
+graph_req_headers = {'content-type': 'application/x-www-form-urlencodeds'}
+#graph_req_body = 'grant_type=client_credentials&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret=' + str(bank_secret)
 
-graph_token_res = requests.post(url=graph_token_uri,headers=graph_req_headers,data=graph_req_body)
+graph_req_body= 'grant_type=client_credentials&client_secret=2fG8Q~LNHsgveTV1FGW8Dg9Esme84ALK9Cm2Pdw2&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default'
+
+graph_token_res = requests.Request("POST",graph_token_uri,headers=graph_req_headers,data=graph_req_body)
 
 print("graph token is \n" +  str(graph_token_res.text))
 
