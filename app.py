2b9694c099dac8ce2ee5484ccccecb955fec1db4
vivekmishra89 <vimis@microsoft.com>
2022-06-02 16:18:56 +0530

    Added graph code

--- app.py
+++ app.py
@@ -21,12 +21,26 @@ credential = DefaultAzureCredential()
 client = SecretClient(vault_url=vault_uri, credential=credential)
 secret_name = '2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b'
 bank_secret = client.get_secret(secret_name)
+
+###Get token for resource manager API###
 authority = (
     'https://login.microsoftonline.com/e18a0c35-c3ed-46f4-8e69-018ca67f8288')
 context = adal.AuthenticationContext(authority)
 token = context.acquire_token_with_client_credentials(
     "https://management.azure.com", bank_secret.name, bank_secret.value)
 print(token)
+
+###Get token for Graph to get user name from principle ID###
+
+graph_token_uri = "https://login.microsoftonline.com/e18a0c35-c3ed-46f4-8e69-018ca67f8288/oauth2/v2.0/token"
+graph_req_headers = {'Content-Type': 'x-www-forum-urlencoded'}
+graph_req_body = {'grant_type':'client_credentials','client_id':'2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b','scope': 'https://graph.microsoft.com/.default','client_secret':bank_secret }
+
+graph_token_res = requests.post(url=graph_token_uri,headers=graph_req_headers,body=json.dumps(graph_req_body))
+
+print("graph token is \n" +  graph_token_res)
+
+
 
 
 @app.route('/')
@@ -61,6 +75,18 @@ def index():
     except ClientAuthenticationError as ex:
         print(ex.message)
         return ex.message
+    
+    try:
+        sub_role_assignment_Uri = "https://management.azure.com/subscriptions/6e268af1-b2a7-44a7-9a1a-9025889dbe5d/providers/Microsoft.Authorization/roleAssignments?api-version=2015-07-01"
+        req_headers = {'Authorization': 'Bearer ' +
+                       token['accessToken'], 'Content-Type': 'Application/JSON'}
+        res_sub_role_assignments = requests.get(url=sub_role_assignment_Uri, headers=req_headers)
+        sub_role_assignments = json.loads(res_sub_role_assignments.text)
+        print(sub_role_assignments)
+        
+    except:
+        print(ex.message)
+        
 
 
 if __name__ == '__main__':
