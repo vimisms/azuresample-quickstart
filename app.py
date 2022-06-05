158cfa9836570e90448b525b3c7392a57244cbcb
vivekmishra89 <vimis@microsoft.com>
2022-06-05 10:06:24 +0530

    testing CSS

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ graph_token_uri = "https://login.microsoftonline.com/e18a0c35-c3ed-46f4-8e69-018
 graph_req_headers = {'content-type': 'application/x-www-form-urlencodeds'}
 graph_req_body= 'grant_type=client_credentials&client_secret=2fG8Q~LNHsgveTV1FGW8Dg9Esme84ALK9Cm2Pdw2&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default'
 graphresponse = requests.request("POST",graph_token_uri,headers=graph_req_headers,data=graph_req_body)
-#print("graph token is \n" +  str(response.text))
+print("graph token is \n" +  str(graphresponse.text))
 
 
 
