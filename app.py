9435b6ca46fd9bcc41f7113790a5479bae7ef89f
vivekmishra89 <vimis@microsoft.com>
2022-06-12 14:46:26 +0530

    Updated code for app service

--- app.py
+++ app.py
@@ -18,13 +18,11 @@ credential = DefaultAzureCredential()
 client = SecretClient(vault_url=vault_uri, credential=credential)
 secret_name = '2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b'
 bank_secret = client.get_secret(secret_name)
-#print("client secret is " + str(bank_secret.value))
 timeStamp = date.today()-timedelta(30)
 ###Get token for resource manager API###
 
 mgmt_token_uri = "https://login.microsoftonline.com/e18a0c35-c3ed-46f4-8e69-018ca67f8288/oauth2/token"
 mgmt_req_headers = {'content-type': 'application/x-www-form-urlencodeds'}
-#mgmt_req_body = 'grant_type=client_credentials&client_secret=2fG8Q~LNHsgveTV1FGW8Dg9Esme84ALK9Cm2Pdw2&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&resource=https%3A%2F%2Fmanagement.azure.com%2F'
 mgmt_req_body = 'grant_type=client_credentials&client_secret='+str(bank_secret.value)+'&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&resource=https%3A%2F%2Fmanagement.azure.com%2F'
 mgmtresponse = requests.request(
     "POST", mgmt_token_uri, headers=mgmt_req_headers, data=mgmt_req_body)
@@ -38,7 +36,6 @@ graph_req_headers = {'content-type': 'application/x-www-form-urlencodeds'}
 graph_req_body = 'grant_type=client_credentials&client_secret='+str(bank_secret.value)+'&client_id=2b0ce5a8-0146-4b0c-a7ef-eccdb99b555b&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default'
 graphresponse = requests.request(
     "POST", graph_token_uri, headers=graph_req_headers, data=graph_req_body)
-#print("graph token is \n" + str(graphresponse.text))
 print("Subscription ID is:\n" + str(os.environ['AZURE_SUBS_ID']))
 
 
