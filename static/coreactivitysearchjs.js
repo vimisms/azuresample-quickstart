91a26cb0f0f42590aa5be63a0bda3bacbbfc7b77
vivekmishra89 <vimis@microsoft.com>
2022-07-01 11:14:32 +0530

    Updated JS for search txt

--- static/coreactivitysearchjs.js
+++ static/coreactivitysearchjs.js
@@ -6,48 +6,51 @@ function searchActivity() {
     tr = table.getElementsByTagName("tr");
     console.log("Able to call JS");
     searchCol = filter.split(";")[0];
+    searchtxt = filter.split(";")[1];
+    console.log(searchCol);
+    console.log(searchtxt);
     console.log(tr.length);
     for (i = 0; i < tr.length; i++) {
-        if (searchCol == "Activity Type") {
+        if (searchCol == "Activity") {
             td = tr[i].getElementsByTagName("td")[0];
             if (td) {
                 txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                if (txtValue.toUpperCase().indexOf(filter.split(";")[1]) > -1) {
                     tr[i].style.display = "";
                 } else {
                     tr[i].style.display = "none";
                 }
             }
-        } else if (searcCol == "Caller") {
+        } else if (searchCol == "Caller") {
             td = tr[i].getElementsByTagName("td")[1];
             console.log("You selected caller filter")
             if (td) {
                 txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                if (txtValue.toUpperCase().indexOf(filter.split(";")[1]) > -1) {
                     tr[i].style.display = "";
                 } else {
                     tr[i].style.display = "none";
                 }
             }
 
-        } else if (searcCol == "Category") {
+        } else if (searchCol == "Category") {
             td = tr[i].getElementsByTagName("td")[2];
             console.log("You selected category filter")
             if (td) {
                 txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                if (txtValue.toUpperCase().indexOf(filter.split(";")[1]) > -1) {
                     tr[i].style.display = "";
                 } else {
                     tr[i].style.display = "none";
                 }
             }
 
-        } else if (searcCol == "Status") {
+        } else if (searchCol == "Status") {
             td = tr[i].getElementsByTagName("td")[6];
             console.log("You selected Status filter")
             if (td) {
                 txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                if (txtValue.toUpperCase().indexOf(filter.split(";")[1]) > -1) {
                     tr[i].style.display = "";
                 } else {
                     tr[i].style.display = "none";
