104e8eff805562c684c4b1522d672ff7b2e0605d
vivekmishra89 <vimis@microsoft.com>
2022-07-01 17:11:00 +0530

    reverted api version

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def compliancecheck():
             storage_account_checks_list.append(storage_account_diag_json.copy())    
             
         for items in res_response['value']:
-            blob_diag_uri = "https://management.azure.com"+items['id']+"/blobServices/default/providers/Microsoft.Insights/diagnosticSettings?api-version=2021-09-01"
+            blob_diag_uri = "https://management.azure.com"+items['id']+"/blobServices/default/providers/Microsoft.Insights/diagnosticSettings?api-version=api-version=2021-05-01-preview"
             print(blob_diag_uri)
             req_headers = {'Authorization': 'Bearer ' + json.loads(mgmtresponse.text)['access_token'], 'Content-Type': 'Application/JSON'}
             res_response = json.loads(requests.get(url=blob_diag_uri, headers=req_headers).text)
@@ -196,7 +196,7 @@ def compliancecheck():
             storage_account_checks_list.append(blob_diag_json.copy()) 
             
         for items in res_response['value']:
-            table_diag_uri = "https://management.azure.com"+items['id']+"/tableServices/default/providers/Microsoft.Insights/diagnosticSettings?api-version=2021-09-01"
+            table_diag_uri = "https://management.azure.com"+items['id']+"/tableServices/default/providers/Microsoft.Insights/diagnosticSettings?api-version=api-version=2021-05-01-preview"
             req_headers = {'Authorization': 'Bearer ' + json.loads(mgmtresponse.text)['access_token'], 'Content-Type': 'Application/JSON'}
             res_response = json.loads(requests.get(url=table_diag_uri, headers=req_headers).text)
             if len(res_response['value']) == 0:
@@ -209,7 +209,7 @@ def compliancecheck():
             storage_account_checks_list.append(table_diag_json.copy())    
             
         for items in res_response['value']:
-            file_diag_uri = "https://management.azure.com"+items['id']+"/fileServices/default/providers/Microsoft.Insights/diagnosticSettings?api-version=2021-09-01"
+            file_diag_uri = "https://management.azure.com"+items['id']+"/fileServices/default/providers/Microsoft.Insights/diagnosticSettings?api-version=api-version=2021-05-01-preview"
             req_headers = {'Authorization': 'Bearer ' + json.loads(mgmtresponse.text)['access_token'], 'Content-Type': 'Application/JSON'}
             res_response = json.loads(requests.get(url=file_diag_uri, headers=req_headers).text)
             if len(res_response['value']) == 0:
