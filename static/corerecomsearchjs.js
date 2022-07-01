a0249b6260982dbf10a3c167b80de448c392f6ca
vivekmishra89 <vimis@microsoft.com>
2022-07-01 13:53:14 +0530

    Updated recom search and button design

---
+++ static/corerecomsearchjs.js
@@ -0,0 +1,37 @@
+function searchActivity() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("recommendationssearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("recomtable");
+    tr = table.getElementsByTagName("tr");
+    searchCol = filter.split(":")[0];
+    searchtxt = filter.split(":")[1];
+
+    for (i = 0; i < tr.length; i++) {
+        if (searchCol === "IMPACT") {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter.split(":")[1]) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+
+        } else if (searchCol === "CATEGORY") {
+            td = tr[i].getElementsByTagName("td")[2];
+            console.log("You selected category filter")
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter.split(":")[1]) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+
+        }
+    }
+
+}
